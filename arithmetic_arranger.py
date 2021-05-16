93353d41701b10b5d419a78b7e0dd92a67ecee5a
Alex Smith <alex.kenneth.smith@gmail.com>
2021-05-16 12:42:28 +0000

    adding error handling

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,14 @@
+import re
 def arithmetic_arranger(problems):
-
+  if len(problems) > 5:
+    raise Exception("Error: Too many problems.")
+  
+  illegal_operators = ["*", "/"]
+  # Could also use regex here...
+  if any([i_o in p for p in problems for i_o in illegal_operators]):
+    raise Exception("Error: Operator must be '+' or '-'.")
+  
+  if any([re.search('[A-Za-Z]+',x) for x in problems]):
+    raise Exception("Error: Numbers must only contain digits")
 
     return arranged_problems
